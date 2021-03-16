8be52fad5857501f02ce5d606af5487fedcb2430
sanyamidha <midhasanya28@gmail.com>
2021-03-16 16:47:41 +0530

    first commit

---
+++ forms/contact.php
@@ -0,0 +1,37 @@
+<?php
+ 
+
+ 
+  $receiving_email_address = 'midhasanya28@gmail.com';
+
+  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+    include( $php_email_form );
+  } else {
+    die( 'Unable to load the "PHP Email Form" Library!');
+  }
+
+  $contact = new PHP_Email_Form;
+  $contact->ajax = true;
+  
+  $contact->to = $receiving_email_address;
+  $contact->from_name = $_POST['name'];
+  $contact->from_email = $_POST['email'];
+  $contact->subject = $_POST['subject'];
+
+  echo $contact->send();
+  $contact->smtp = array(
+    'host' => 'midhasanya28@gmail.com',
+    'username' => '',
+    'password' => 'pass',
+    'port' => '587'
+    
+  );
+
+
+  $contact->add_message( $_POST['name'], 'From');
+  $contact->add_message( $_POST['email'], 'Email');
+  $contact->add_message( $_POST['message'], 'Message', 10);
+
+  
+  
+?>
